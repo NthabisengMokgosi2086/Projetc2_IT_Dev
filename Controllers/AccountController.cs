0050e026ab63b28e04b6a5a4dacf15b6ff3b508b
Your Name <you@example.com>
2020-10-27 18:33:52 +0200

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Projetc2_IT_Dev.Models; ///reference to model
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Web;
+using Projetc2_IT_Dev.Controllers;
+
+
+namespace Projetc2_IT_Dev.Controllers
+{
+    public class AccountController : Controller
+    {
+        
+        //Get: Account  
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        void connectionString()
+        {
+
+        }
+        public IActionResult Verify(Account acc)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projetc2_IT_Dev.Models
+{
+    public class Account
+    {
+
+        public string EmployeeNumber{ get; set; }
+
+        public string Department { get; set; }
+
+        public string Role { get; set; }
+
+        public string MaritalStatus { get; set; }
+
+        public string Gender { get; set; }
+
+        public string BusinessTravel { get; set; }
+
+        public string EducationField { get; set; }
+
+
+
+
+    }
+}
